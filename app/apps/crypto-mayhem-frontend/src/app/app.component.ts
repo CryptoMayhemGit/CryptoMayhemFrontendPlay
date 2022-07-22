d1d0eb9b1c84ce3002782e89e5a16c4ef03e3403
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-07-23 01:35:50 +0200

    Revert ngif for wallet modal

--- app/apps/crypto-mayhem-frontend/src/app/app.component.ts
+++ app/apps/crypto-mayhem-frontend/src/app/app.component.ts
@@ -7,7 +7,9 @@ import { WalletFacade } from 'libs/crypto-mayhem/data-access/wallet/src/lib/faca
     <ui-nav></ui-nav>
     <router-outlet></router-outlet>
     <ui-notification></ui-notification>
-    <ui-wallet-choice></ui-wallet-choice>
+    <ui-wallet-choice
+      *ngIf="walletFacade.showWallets$ | async"
+    ></ui-wallet-choice>
   `,
 })
 export class AppComponent {
