3a727251a8ae37ca215f4efcde6d640d7806bd12
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2022-07-25 13:35:25 +0200

    fix

--- app/libs/ui/src/lib/pre-sale/pre-sale.module.ts
+++ app/libs/ui/src/lib/pre-sale/pre-sale.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WalletEffects } from '@crypto-mayhem-frontend/crypto-mayhem/data-access/wallet';
 import { TranslocoModule } from '@ngneat/transloco';
 import { EffectsModule } from '@ngrx/effects';
-import { InputModule } from '../input/input.module';
 import { MainButtonModule } from '../main-button/main-button.module';
 import { ProgressBarModule } from '../progress-bar/progress-bar.module';
 import { PreSaleComponent } from './pre-sale.component';
