7fa77a63c26452e4931b90bb49990bf65dcaf60c
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2022-07-27 20:13:18 +0200

    add env to prod and stage

--- app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,14 @@
 export const environment = {
   debug: false,
   production: true,
-  externalUrl: '',
-  baseUrl: '',
-  rpcUrl: '',
-  rpcChainId: '',
-  defaultAsset: '',
+  externalUrl: "http://localhost:4200/",
+  baseUrl: "https://mayhemwebapi.azurewebsites.net/",
+  //rpcUrl: "http://localhost:8545/",
+  rpcChainId: "0x539",
+  defaultAsset: "ADRIAT",
+
+  rpcUrlBinance: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+  chainIdNumberBinance: 97, //Binance Smart Chain
+  chainIdHexBinance: '0x61', //Binance Smart Chain
+  networkId: '' //Binance Smart Chain
 };
