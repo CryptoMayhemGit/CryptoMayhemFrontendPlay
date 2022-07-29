40f9baa680729647a026df41d0184af786834310
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2022-07-29 16:55:26 +0200

    remove contracts addresses

--- app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   networkId: '',
   stage: 0, //Binance Smart Chain
   maxNumberOfUsdcPerStage: 1000,
-  usdcContractAddress: '0x6D9e2BAAAe74E348254F507b3d536A084F9ecdbA',
-    adriaContractAddress: '0x1Cb96bbEae0227eE8840f78b2158B5316ff00BE9',
-    adriaVestingContractAddress: '0x5dD02ca1eBa2FE73f71B1895931389E8cAB557a2'
+  usdcContractAddress: '',
+    adriaContractAddress: '',
+    adriaVestingContractAddress: ''
 };
