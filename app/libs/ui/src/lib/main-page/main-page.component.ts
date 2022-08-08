920ea958bb044e50996440acc0d3c063ab50ae69
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-08-08 11:33:43 +0200

    CSS and structure changes regarding home page

--- app/libs/ui/src/lib/main-page/main-page.component.ts
+++ app/libs/ui/src/lib/main-page/main-page.component.ts
@@ -1,16 +1,41 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { AbsoluteSourceSpan } from '@angular/compiler';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ui-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('droneIn', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translate(-300%, 100%)',
+        }),
+        animate(
+          '1000ms ease-in',
+          style({ opacity: 1, transform: 'translate(0)' })
+        ),
+      ]),
+    ]),
+    trigger('slideUp', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(50%)',
+        }),
+        animate(
+          '500ms ease-in',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class MainPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
