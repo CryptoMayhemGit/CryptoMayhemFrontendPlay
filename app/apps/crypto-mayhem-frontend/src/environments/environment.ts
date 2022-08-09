2e4decef94bafd23f237ae2bfec86a24dcde3b52
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-08-09 14:30:23 +0200

    Changed contracts, abis and env settings

--- app/apps/crypto-mayhem-frontend/src/environments/environment.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.ts
@@ -17,11 +17,11 @@ export const environment = {
   chainIdHexBinance: '0x61', //Binance Smart Chain
   networkId: '', //Binance Smart Chain
   stage: 0,
-  maxNumberOfUsdcPerStage: 150,
-  usdcContractAddress: '0x6D9e2BAAAe74E348254F507b3d536A084F9ecdbA',
-  adriaContractAddress: '0x1Cb96bbEae0227eE8840f78b2158B5316ff00BE9',
-  adriaVestingContractAddress: '0x5dD02ca1eBa2FE73f71B1895931389E8cAB557a2',
-  adriaPrice: 0.0016,
+  maxNumberOfUsdcPerStage: 400,
+  usdcContractAddress: '0x1bf5f3dc3e45f6db312f206bed38e1225d9fe9f8',
+  adriaContractAddress: '0xe6482247cfe9a9eb23fb84ff002674f7d8cba33c',
+  adriaVestingContractAddress: '0x4dd7f9b6b5f2f2078d6bd79a1624ec89bf0374b2',
+  adriaPrice: 0.002,
   webGlLocation: 'assets/unity/',
   metamaskDeepLink:
     'https://metamask.app.link/dapp/black-mushroom-0ae7fe803-develop.westeurope.1.azurestaticapps.net/presale',
