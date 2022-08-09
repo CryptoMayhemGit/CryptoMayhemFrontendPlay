43bebfb766cc76276bf3338941ac12e15682790f
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-08-09 11:51:56 +0200

    Changes regarding main play page

--- app/libs/ui/src/lib/video-component/video-component.component.ts
+++ app/libs/ui/src/lib/video-component/video-component.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'ui-video-component',
@@ -7,7 +15,28 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VideoComponentComponent implements OnInit {
-  constructor() {}
+  @Input() title: string = '';
+  @Input() btnSecondaryTxt!: string;
+  @Input() btnPrimaryTxt!: string;
+  @Input() videoId!: string;
 
-  ngOnInit(): void {}
+  @Output() primaryClickEvent = new EventEmitter();
+  @Output() secondaryClickEvent = new EventEmitter();
+
+  videoUrl!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    const url = `https://www.youtube.com/embed/${this.videoId}?autoplay=0&amp;showinfo=0&amp;controls=1&amp;iv_load_policy=3&amp;modestbranding=1&amp;rel=0`;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  primaryClick() {
+    this.primaryClickEvent.emit();
+  }
+
+  secondaryClick() {
+    this.secondaryClickEvent.emit();
+  }
 }
