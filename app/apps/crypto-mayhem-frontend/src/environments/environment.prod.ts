53f6c5afc88d748ddab50a4b16cc9e43abf4b0c6
Przemysław Chabasiński <chabasinski.przemyslaw@adriagames.com>
2022-08-26 15:57:23 +0200

    change adria vesting contract adress

--- app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
@@ -16,7 +16,7 @@ export const environment = {
   maxNumberOfUsdcPerStage: 400,
   usdcContractAddress: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
   adriaContractAddress: '0x2Ae411D012fEeA14d2d1305550a99Fcf82FB8594',
-  adriaVestingContractAddress: '0x73d18c8675cCafA22865Ac9D5A02cFCf3a8e679c',
+  adriaVestingContractAddress: '0xBbD93569C664ce6FBA65B4B7f36BA93B1E8C7a86',
   adriaPrice: 0.002,
   webGlLocation: '',
   metamaskDeepLink:
