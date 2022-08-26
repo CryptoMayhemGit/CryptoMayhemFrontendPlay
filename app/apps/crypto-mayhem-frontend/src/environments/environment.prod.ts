cad4d59ff0daddfb6694905ee3872c248aba2177
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-08-26 08:24:44 +0200

    changes for production release

--- app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.prod.ts
@@ -13,11 +13,11 @@ export const environment = {
   chainIdHexBinance: '0x38', //Binance Smart Chain
   networkId: '',
   stage: 0, //Binance Smart Chain
-  maxNumberOfUsdcPerStage: 150,
+  maxNumberOfUsdcPerStage: 400,
   usdcContractAddress: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
   adriaContractAddress: '0x2Ae411D012fEeA14d2d1305550a99Fcf82FB8594',
   adriaVestingContractAddress: '0x73d18c8675cCafA22865Ac9D5A02cFCf3a8e679c',
-  adriaPrice: 0.0016,
+  adriaPrice: 0.002,
   webGlLocation: '',
   metamaskDeepLink:
     'https://metamask.app.link/dapp/play.cryptomayhem.io/presale',
