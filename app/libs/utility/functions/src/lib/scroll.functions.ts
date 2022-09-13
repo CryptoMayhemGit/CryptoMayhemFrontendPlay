961d069763ef3e14c50cb604fafdf0ef11b9093b
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-09-13 20:06:38 +0200

    Focus element before scrolling to

--- app/libs/utility/functions/src/lib/scroll.functions.ts
+++ app/libs/utility/functions/src/lib/scroll.functions.ts
@@ -1,5 +1,6 @@
 export function scrollTo(elementId: string): void {
   const el = document.getElementById(elementId);
+  el?.focus();
   el?.scrollIntoView({
     behavior: 'smooth',
     block: 'start',
