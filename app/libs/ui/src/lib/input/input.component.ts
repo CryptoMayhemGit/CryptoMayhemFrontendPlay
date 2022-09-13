67061203cd5bb5bc4dca03c27883d8c44434c22d
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-09-13 20:06:04 +0200

    Control value accessor for input component

--- app/libs/ui/src/lib/input/input.component.ts
+++ app/libs/ui/src/lib/input/input.component.ts
@@ -1,18 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'ui-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, ControlValueAccessor {
   @Input() type: string = 'text';
+  @Input() name: string = '';
   @Input() value: string | number | null = null;
   @Input() placeholder: string = '';
   @Input() iconLeft!: IconDefinition;
 
+  public readonly inputControl = new FormControl();
+
+  private _onChange = (value: string | null) => undefined;
+  private _onTouched = () => undefined;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inputControl.valueChanges.subscribe((val) => {
+      const fieldsAreValid = this.inputControl.valid;
+      const value = fieldsAreValid ? val : null;
+
+      this._onChange(value);
+      this._onTouched();
+    });
+  }
+
+  registerOnChange(fn: (value: string | null) => undefined) {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: () => undefined) {
+    this._onTouched = fn;
+  }
+
+  writeValue(value: string | null): void {
+    this.inputControl.setValue(value);
+  }
 }
--- app/libs/ui/src/lib/input/input.module.ts
+++ app/libs/ui/src/lib/input/input.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { forwardRef, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input.component';
+import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [InputComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [InputComponent]
 })
