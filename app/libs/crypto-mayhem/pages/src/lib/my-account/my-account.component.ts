ee20058c05e3b84c6a6def321fcea218ed36e4e2
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-09-15 04:18:47 +0200

    Added loot aection

--- app/libs/crypto-mayhem/pages/src/lib/my-account/my-account.component.ts
+++ app/libs/crypto-mayhem/pages/src/lib/my-account/my-account.component.ts
@@ -51,6 +51,8 @@ export class MyAccountComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    this.setNftSection('loot');
+
     if (isFullHD()) {
       this.submenu = true;
     }
