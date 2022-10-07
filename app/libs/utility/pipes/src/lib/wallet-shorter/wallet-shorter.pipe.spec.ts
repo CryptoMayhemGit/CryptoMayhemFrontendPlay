d46bf04e9f8dd7bc344ab93c0234d5ce28c5be53
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2022-10-07 10:30:48 +0200

    [CMW-376] fix unit test for wallet shorter pipe

--- app/libs/utility/pipes/src/lib/wallet-shorter/wallet-shorter.pipe.spec.ts
+++ app/libs/utility/pipes/src/lib/wallet-shorter/wallet-shorter.pipe.spec.ts
@@ -8,9 +8,9 @@ describe('FormatTimePipe', () => {
   
     it('should transform wallet address into shorter form', () => {
       const pipe = new WalletShorterPipe();
-      expect('0xd20A336057A940BCae44554B1B5CbC2C716bED5d').toBe('0xd2...ED5d');
-      expect('').toBe('');
-      expect('0xd20A336057A940BCae44554B1B5CbC212345678').toBe('0xd2...5678');
-      expect('00000000000000000000000000000000000000000').toBe('0000...0000');
+      expect(pipe.transform('0xd20A336057A940BCae44554B1B5CbC2C716bED5d')).toBe('0xd2...ED5d');
+      expect(pipe.transform('')).toBe('');
+      expect(pipe.transform('0xd20A336057A940BCae44554B1B5CbC212345678')).toBe('0xd2...5678');
+      expect(pipe.transform('00000000000000000000000000000000000000000')).toBe('0000...0000');
     });
   });
