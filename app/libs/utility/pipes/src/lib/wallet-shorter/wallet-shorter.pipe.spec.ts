51fce24ad447be1708ef86ddd74dad2732d2894a
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2022-10-07 09:43:11 +0200

    [CMW-376] unit test, refactor code

---
+++ app/libs/utility/pipes/src/lib/wallet-shorter/wallet-shorter.pipe.spec.ts
@@ -0,0 +1,16 @@
+import { WalletShorterPipe } from "./wallet-shorter.pipe";
+
+describe('FormatTimePipe', () => {
+    it('create an instance', () => {
+      const pipe = new WalletShorterPipe();
+      expect(pipe).toBeTruthy();
+    });
+  
+    it('should transform wallet address into shorter form', () => {
+      const pipe = new WalletShorterPipe();
+      expect('0xd20A336057A940BCae44554B1B5CbC2C716bED5d').toBe('0xd2...ED5d');
+      expect('').toBe('');
+      expect('0xd20A336057A940BCae44554B1B5CbC212345678').toBe('0xd2...5678');
+      expect('00000000000000000000000000000000000000000').toBe('0000...0000');
+    });
+  });
