11db03c166c2175bc7998468abf41b11d810f8d9
Emilia Heller-Mrotek <emkacf@gmail.com>
2022-10-09 14:25:42 +0200

    Accept null as a value for bnb pipe

--- app/libs/utility/pipes/src/lib/bnb-token/bnb-token.pipe.ts
+++ app/libs/utility/pipes/src/lib/bnb-token/bnb-token.pipe.ts
@@ -4,7 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'bnbToken',
 })
 export class BnbTokenPipe implements PipeTransform {
-  transform(value: number): string {
+  transform(value: number | null): string {
+    if (value === null) {
+      return '';
+    }
+
     let amount = value.toString();
     return amount.length > 7 ? amount.substring(0, 7) : amount;
   }
