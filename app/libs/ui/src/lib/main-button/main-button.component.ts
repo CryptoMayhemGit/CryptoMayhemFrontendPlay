05edeaf2256ace8e29bae7eca01099f4376b7064
Emilia Heller-Mrotek <>
2022-10-18 08:39:48 +0200

    Fixes for mobile my account initial component

--- app/libs/ui/src/lib/main-button/main-button.component.ts
+++ app/libs/ui/src/lib/main-button/main-button.component.ts
@@ -7,8 +7,6 @@ import {
 } from '@angular/core';
 
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
-import { WalletFacade } from 'libs/crypto-mayhem/data-access/wallet/src/lib/facades/wallet.facade';
-import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'ui-main-button',
