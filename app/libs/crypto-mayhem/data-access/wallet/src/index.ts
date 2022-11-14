d2f0d3876ce2e76a26abe0e2fc6243ae3ee9f779
Emilia Heller-Mrotek <>
2022-11-14 19:08:44 +0700

    Created initial launcher page

--- app/libs/crypto-mayhem/data-access/wallet/src/index.ts
+++ app/libs/crypto-mayhem/data-access/wallet/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/crypto-mayhem-data-access-wallet.module';
 export * from './lib/state/wallet.effects';
+export * from './lib/state/wallet.reducer';
+export * from './lib/facades/wallet.facade'
---
+++ app/libs/crypto-mayhem/pages/src/lib/launcher/launcher.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { WalletFacade } from '@crypto-mayhem-frontend/crypto-mayhem/data-access/wallet';
+import { Observable, tap } from 'rxjs';
+
+@Component({
+  templateUrl: './launcher.component.html',
+  styleUrls: ['./launcher.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LauncherComponent implements OnInit {
+
+  account$!: Observable<string>
+
+  constructor(
+    private walletFacade: WalletFacade
+  ) {
+    this.account$ = this.walletFacade.account$
+  }
+
+  ngOnInit(): void {
+    this.walletFacade.showWallets();
+
+    this.account$.pipe(
+      tap(
+        (account) => {
+          console.log(account);
+        }
+      )
+    ).subscribe();
+  }
+
+}
