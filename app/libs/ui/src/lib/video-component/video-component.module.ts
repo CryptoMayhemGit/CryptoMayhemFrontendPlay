f48a35c7c8882c4e91ecf4142d180a8638188133
Emilia Heller-Mrotek <emkacf@gmail.com>
2023-03-15 16:24:17 +0100

    feat: changes on main page with video component

--- app/libs/ui/src/lib/video-component/video-component.module.ts
+++ app/libs/ui/src/lib/video-component/video-component.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoComponentComponent } from './video-component.component';
 import { MainButtonModule } from '../main-button/main-button.module';
+import { TooltipModule } from '../tooltip/tooltip.module';
 
 @NgModule({
   declarations: [VideoComponentComponent],
-  imports: [CommonModule, MainButtonModule],
+  imports: [CommonModule, MainButtonModule, TooltipModule],
   exports: [VideoComponentComponent],
 })
 export class VideoComponentModule {}
