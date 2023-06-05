12ae0b3a0e31c57d311a7a5c82791ede16a80ecd
chabasinskiprzemyslaw <chabasinski.przemyslaw@gmail.com>
2023-06-05 09:18:36 +0200

    API Key do Face Wallet

--- app/apps/crypto-mayhem-frontend/src/environments/environment.dev.ts
+++ app/apps/crypto-mayhem-frontend/src/environments/environment.dev.ts
@@ -21,5 +21,5 @@ export const environment = {
     'https://metamask.app.link/dapp/black-mushroom-0ae7fe803-develop.westeurope.1.azurestaticapps.net/presale',
   domain: 'cryptomayhem.io',
   cyberConnectUrl: 'https://api.cyberconnect.dev/',
-  faceWalletAPIKey: '',
+  faceWalletAPIKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjI-z36LF-P2v9MgjNAjV75QEwOTKshmIfIJ5cQfRWXJSZY6dvDphv9AJIT5axikomB9G7ACZqLW84R7mHuq9K4IgXAj0j_dKMp19RiCnjiF-XuKV0uRocW3albDvA5bZ8JU_M7wdNZS2GdfEHBcP7OLSs6CmANKywTqOaL-08fwIDAQAB',
 };
