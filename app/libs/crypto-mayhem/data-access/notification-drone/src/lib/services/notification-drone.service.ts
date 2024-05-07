b5fd4f657d61a538e7750ce930c64d0ed40e9722
Paweł Spionkowski <pawel.spionkowski@gmail.com>
2024-05-07 21:19:25 +0200

    Last warrning has been fixed.

--- app/libs/crypto-mayhem/data-access/notification-drone/src/lib/services/notification-drone.service.ts
+++ app/libs/crypto-mayhem/data-access/notification-drone/src/lib/services/notification-drone.service.ts
@@ -4,19 +4,28 @@ import { error, hide, success, info } from '../state/notification-drone.actions'
 import { Url } from '../state/notification-drone.reducer';
 
 import * as NotificationDroneSelectors from '../state/notification-drone.selectors';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationsService {
-  readonly show$ = this.store.select(NotificationDroneSelectors.getShow);
-  readonly type$ = this.store.select(NotificationDroneSelectors.getType);
-  readonly title$ = this.store.select(NotificationDroneSelectors.getTitle);
-  readonly message$ = this.store.select(NotificationDroneSelectors.getMessage);
-  readonly textLink$ = this.store.select(NotificationDroneSelectors.getTextLink);
+  public show$: Observable<boolean>;
+  public type$: Observable<string>;
+  public title$: Observable<string>;
+  public message$: Observable<string | undefined>;
+  public textLink$: Observable<Url | undefined>;
+  
+  constructor(private readonly store: Store) 
+  {
+    this.show$ = this.store.select(NotificationDroneSelectors.getShow);
+    this.type$ = this.store.select(NotificationDroneSelectors.getType);
+    this.title$ = this.store.select(NotificationDroneSelectors.getTitle);
+    this.message$ = this.store.select(NotificationDroneSelectors.getMessage);
+    this.textLink$ = this.store.select(NotificationDroneSelectors.getTextLink);
+  }
+
   // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly TIMEOUT = 5000;
   private timeout: any;
-
-  constructor(private readonly store: Store) {}
 
   public error(title: string, message?: string, textLink?: Url, autoclose?: boolean,): void {
     this.store.dispatch(error({ title, message, textLink }));
--- app/libs/crypto-mayhem/data-access/vesting/src/lib/facades/vesting.facade.ts
+++ app/libs/crypto-mayhem/data-access/vesting/src/lib/facades/vesting.facade.ts
@@ -5,14 +5,18 @@ import * as fromVesting from '../state/vesting.reducer';
 import * as VestingSelectors from '../state/vesting.selectors';
 
 import { selectAppSignedIn } from '../state/vesting.selectors';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class VestingFacade {
-    signedIn$ = this.store.pipe(select(selectAppSignedIn));
-    readonly vestingLargeSpinnerActive$ = this.store.select(VestingSelectors.selectVestingLargeSpinnerActive);
+    public signedIn$: Observable<string>;
+    public vestingLargeSpinnerActive$: Observable<boolean>;
 
     constructor(
         private readonly store: Store<fromVesting.VestingState>,
-    ) {
+    )
+    {
+        this.signedIn$ = this.store.pipe(select(selectAppSignedIn));
+        this.vestingLargeSpinnerActive$ = this.store.select(VestingSelectors.selectVestingLargeSpinnerActive);
     }
 }
